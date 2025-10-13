afcd842bc6caa657ec1d6ad0e8323eaa1e672c14
Adnan Ansari <adnanansari.aa.114@gmail.com>
2025-10-13 11:20:15 +0530

    your message

--- src/socket.js
+++ src/socket.js
@@ -1,5 +1,5 @@
 import { io } from "socket.io-client";
-const URL = import.meta.env.VITE_SOCKET_URL || import.meta.env.VITE_API_BASE_URL || window.location.origin;
+const URL = 'http://localhost:5000';
 export const socket = io(URL, { autoConnect: true });
 
 
--- src/utils/api.jsx
+++ src/utils/api.jsx
@@ -1,8 +1,8 @@
-src/utils/api.js
+// src/utils/api.js
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL, // 🔹 yaha apna backend ka base URL lagana
+  baseURL: 'http://localhost:5000', // 🔹 yaha apna backend ka base URL lagana
   headers: {
     "Content-Type": "application/json",
   }, 
@@ -24,7 +24,7 @@ export default API;
 
 
 
-import axios from "axios";
+// import axios from "axios";
 
 // const API = axios.create({
 //   baseURL: "http://localhost:5000", // 🔹 yaha apna backend ka base URL lagana
